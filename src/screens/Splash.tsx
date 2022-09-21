5cac14ce136d45afb6e409a95bc138dde002c488
comartek <longpq@comartek.com>
2022-09-21 17:34:57 +0700

    Update Splash

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,18 +1,25 @@
 import {Image, SafeAreaView, Text, View} from 'react-native';
 import {colors} from '../assets/colors';
+import LinearGradient from 'react-native-linear-gradient';
 
 const Splash: React.FC = () => {
   return (
     <SafeAreaView>
-      <View
-        className={`flex-1`}
-        style={{backgroundColor: colors.primaryGradiant}}>
-        <Image
-          className="h-32 w-32"
-          source={require('../assets/logoTinwin.png')}
-        />
-      </View>
-      {/* <Text>Splash</Text> */}
+      <LinearGradient
+        colors={['#FD7D00', '#FEB336']}
+        start={{x: 0, y: 0.5}}
+        end={{x: 1, y: 0.5}}
+        locations={[0, 1]}>
+        <View className="flex-1 h-full items-center justify-center">
+          <Image
+            className="h-48 w-60 "
+            source={require('../assets/logoTinwin.png')}
+          />
+          <Text className="text-white font-bold text-2xl">
+            Xoá mọi khoảng cách
+          </Text>
+        </View>
+      </LinearGradient>
     </SafeAreaView>
   );
 };
